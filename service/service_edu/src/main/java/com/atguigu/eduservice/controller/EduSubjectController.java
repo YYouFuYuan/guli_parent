329b386dc910cabfe17563560a9cabf81f4deec2
YYouFuYuan <1098091827@qq.com>
2022-03-04 20:25:10 +0800

    2022/3/4  项目第四天 excel生成分类

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -0,0 +1,38 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.service.EduSubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * <p>
+ * 课程科目 前端控制器
+ * </p>
+ *
+ * @author Ay
+ * @since 2022-03-04
+ */
+@RestController
+@RequestMapping("/eduservice/subject")
+@CrossOrigin
+public class EduSubjectController {
+
+    @Autowired
+    private EduSubjectService eduSubjectService;
+
+
+    @PostMapping("addSubject")
+    public R addSubject(MultipartFile file){
+        eduSubjectService.saveSubject(file,eduSubjectService);
+        return R.ok();
+    }
+}
+
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -0,0 +1,45 @@
+package com.atguigu.eduservice.service.impl;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.listener.SubjectExcelListener;
+import com.atguigu.eduservice.mapper.EduSubjectMapper;
+import com.atguigu.eduservice.service.EduSubjectService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * <p>
+ * 课程科目 服务实现类
+ * </p>
+ *
+ * @author Ay
+ * @since 2022-03-04
+ */
+@Service
+public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubject> implements EduSubjectService {
+
+    /**
+     * 添加课程分类
+     * @param file
+     */
+    @Override
+    public void saveSubject(MultipartFile file,EduSubjectService eduSubjectService) {
+        try {
+            InputStream inputStream = file.getInputStream();
+            EasyExcel.read(inputStream, SubjectData.class, new SubjectExcelListener(this)).sheet().doRead();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
